761a77851e4d3259ec2b7310eb51c2f141912733
Orujhimaru <159823243+Orujhimaru@users.noreply.github.com>
2024-07-19 00:40:30 +0300

    [Main Page Setup]

--- app.js
+++ app.js
@@ -3,21 +3,77 @@
 //   x: 500,
 // });
 
-var lastScrollTop = document.pageYOffset;
+// var lastScrollTop = document.pageYOffset;
 
-// element should be replaced with the actual target element on which you have applied scroll, use window in case of no target element.
-element.addEventListener(
-  "scroll",
-  function () {
-    // or window.addEventListener("scroll"....
-    var st = window.pageYOffset || document.documentElement.scrollTop; // Credits: "https://github.com/qeremy/so/blob/master/so.dom.js#L426"
-    if (st > lastScrollTop) {
-      // downscroll code
-    } else if (st < lastScrollTop) {
-      // upscroll code
-    } // else was horizontal scroll
-    lastScrollTop = st <= 0 ? 0 : st; // For Mobile or negative scrolling
-    console.log(lastScrollTop);
+// // element should be replaced with the actual target element on which you have applied scroll, use window in case of no target element.
+// element.addEventListener(
+//   "scroll",
+//   function () {
+//     // or window.addEventListener("scroll"....
+//     var st = window.pageYOffset || document.documentElement.scrollTop; // Credits: "https://github.com/qeremy/so/blob/master/so.dom.js#L426"
+//     if (st > lastScrollTop) {
+//       // downscroll code
+//     } else if (st < lastScrollTop) {
+//       // upscroll code
+//     } // else was horizontal scroll
+//     lastScrollTop = st <= 0 ? 0 : st; // For Mobile or negative scrolling
+//     console.log(lastScrollTop);
+//   },
+//   false
+// );
+// WebGLSampler.registerPlugin(scrollTrigger);
+
+// ScrollTrigger.defaults({
+//   toggleActions: "restart pause resume pause",
+//   scroller: ".container"
+// });
+
+// gsap.to(
+
+// )
+gsap.registerPlugin(ScrollTrigger);
+
+gsap.to("container", {
+  duration: 2,
+  y: -100,
+});
+
+gsap.to(".absolute-text-1", {
+  scrollTrigger: {
+    scroller: ".container",
+    trigger: ".absolute-text-1",
+    toggleActions: "restart none none none",
   },
-  false
-);
+  duration: 2,
+  opacity: 1,
+});
+
+gsap.to(".absolute-text-2", {
+  scrollTrigger: {
+    scroller: ".container",
+    trigger: ".absolute-text-2",
+    toggleActions: "restart none none pause",
+  },
+  duration: 2,
+  opacity: 1,
+});
+
+gsap.to(".absolute-text-3", {
+  scrollTrigger: {
+    scroller: ".container",
+    trigger: ".absolute-text-3",
+    toggleActions: "restart none none none",
+  },
+  duration: 2,
+  opacity: 1,
+});
+
+gsap.to(".absolute-text-4", {
+  scrollTrigger: {
+    scroller: ".container",
+    trigger: ".absolute-text-4",
+    toggleActions: "restart none none none",
+  },
+  duration: 2,
+  opacity: 1,
+});
