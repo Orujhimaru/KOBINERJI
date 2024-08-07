f9ca093ef24f2562c1764a13ac753723048d7c88
Orujhimaru <159823243+Orujhimaru@users.noreply.github.com>
2024-08-07 15:05:41 +0300

    Blogs page animation

---
+++ blog.js
@@ -0,0 +1,35 @@
+const loader = document.getElementById("preloader");
+window.addEventListener("load", () =>
+  setTimeout(function () {
+    hideLoader();
+  }, 2000)
+);
+
+function hideLoader() {
+  loader.style.display = "none";
+}
+
+const blogs = document.getElementById("blog-all");
+
+blogs.addEventListener("click", function () {
+  loadNewPage();
+});
+
+//   // change conatiner to page1 - to sections
+
+function loadNewPage() {
+  gsap.to(".blog-area", {
+    opacity: 0,
+    duration: 1,
+    onComplete: () => {
+    //   container.classList.toggle("hide-scrolling");
+    //   slider.classList.toggle("hide-item");
+    //   localStorage.setItem("pageNumber", pageNumber.toString());
+    //   window.location.href = "/hizmetler.html";
+      // const body = document.getElementById("body");
+      // body.innerHTML += addPageContent(pageNumber);
+      // addPageContent(pageNumber); ADD IT LATER ON**
+      DISPLAY FROM NONE TO BLOCK ***.
+    },
+  });
+}
