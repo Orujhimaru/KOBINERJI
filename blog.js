f4f88e780de5002956e544bea1ce206c5af5b5d0
Orujhimaru <159823243+Orujhimaru@users.noreply.github.com>
2024-08-07 17:13:36 +0300

    animation for blog

--- blog.js
+++ blog.js
@@ -9,33 +9,34 @@ function hideLoader() {
   loader.style.display = "none";
 }
 
-// const blogs = document.getElementById("blog-all");
-// const page1 = document.getElementById("blog1");
-// const page2 = document.getElementById("blog2");
-// const page3 = document.getElementById("blog3");
+const blogs = document.getElementById("blog-all");
+const page1 = document.getElementById("blog1");
+const page2 = document.getElementById("blog2");
+const page3 = document.getElementById("blog3");
 
-// const arrOfBlogs = [page1, page2, page3];
-// for (let i = 0; i < arrOfBlogs.length; i++) {
-//   arrOfPages[i].addEventListener("click", function () {
-//     loadNewPage(i);
-//   });
-// }
+const arrOfBlogs = [page1, page2, page3];
+for (let i = 0; i < arrOfBlogs.length; i++) {
+  arrOfBlogs[i].addEventListener("click", function () {
+    loadNewPage();
+  });
+}
 
 // //   // change conatiner to page1 - to sections
 
-// function loadNewPage() {
-//   gsap.to(".blog-area", {
-//     opacity: 0,
-//     duration: 1,
-//     onComplete: () => {
-//       //   container.classList.toggle("hide-scrolling");
-//       //   slider.classList.toggle("hide-item");
-//       //   localStorage.setItem("pageNumber", pageNumber.toString());
-//       //   window.location.href = "/hizmetler.html";
-//       // const body = document.getElementById("body");
-//       // body.innerHTML += addPageContent(pageNumber);
-//       // addPageContent(pageNumber); ADD IT LATER ON**
-//       // DISPLAY FROM NONE TO BLOCK ***.
-//     },
-//   });
-// }
+function loadNewPage() {
+  gsap.to(".blog-area", {
+    opacity: 0,
+    display: "none",
+    duration: 1,
+    onComplete: () => {
+      //   container.classList.toggle("hide-scrolling");
+      //   slider.classList.toggle("hide-item");
+      //   localStorage.setItem("pageNumber", pageNumber.toString());
+      //   window.location.href = "/hizmetler.html";
+      // const body = document.getElementById("body");
+      // body.innerHTML += addPageContent(pageNumber);
+      // addPageContent(pageNumber); ADD IT LATER ON**
+      // DISPLAY FROM NONE TO BLOCK ***.
+    },
+  });
+}
