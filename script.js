bf242649fed1980fec8fc4712bbaa056700bb24d
Orujhimaru <159823243+Orujhimaru@users.noreply.github.com>
2024-08-10 14:35:53 +0300

    privacy

---
+++ script.js
@@ -0,0 +1,27 @@
+const consentBox = document.getElementById("consentBox");
+const acceptBtn = document.querySelector(".consentButton");
+const rejectBtn = document.querySelector(".rejectButton");
+
+acceptBtn.onclick = () => {
+  document.cookie = "cookieBy= kobinerji; max-age=" + 60 * 60 * 24;
+  if (document.cookie) {
+    consentBox.classList.add("hide");
+  } else {
+    alert(
+      "Cookie can't be set! Please" +
+        " unblock this site from the cookie" +
+        " setting of your browser."
+    );
+  }
+};
+
+rejectBtn.onclick = () => {
+  alert("Cookies rejected. Some functionality may be limited.");
+  consentBox.classList.add("hide");
+};
+
+let checkCookie = document.cookie.includes("kobinerji");
+console.log(checkCookie);
+checkCookie !== false
+  ? consentBox.classList.add("hide")
+  : consentBox.classList.remove("hide");
