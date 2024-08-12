67b097d0a0cf605d5fdde9e73132b0bd813a859d
Orujhimaru <159823243+Orujhimaru@users.noreply.github.com>
2024-08-12 22:06:02 +0300

    time

--- app.js
+++ app.js
@@ -1,5 +1,21 @@
 // reload window on going back js
-
+function reloadPage() {
+  // The last "domLoading" Time //
+  var currentDocumentTimestamp = new Date(
+    performance.timing.domLoading
+  ).getTime();
+  // Current Time //
+  var now = Date.now();
+  // Ten Seconds //
+  var tenSec = 10 * 1000;
+  // Plus Ten Seconds //
+  var plusTenSec = currentDocumentTimestamp + tenSec;
+  if (now > plusTenSec) {
+    location.reload();
+  } else {
+  }
+}
+reloadPage();
 var localStorage = window.localStorage;
 gsap.registerPlugin(ScrollTrigger);
 
